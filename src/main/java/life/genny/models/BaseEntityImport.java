8f0d2def01b5e16e1cec9f2c03da2ca733bc7ca0
Adam Crow <adamcrow63@gmail.com>
2020-01-13 13:29:54 +1100

    Updated with import utils

---
+++ src/main/java/life/genny/models/BaseEntityImport.java
@@ -0,0 +1,70 @@
+package life.genny.models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.vavr.Tuple2;
+
+public class BaseEntityImport implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String code;
+	String name;
+	
+	List<Tuple2<String,String>> attributeValuePairList = new ArrayList<Tuple2<String,String>>();
+
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the attributeValuePairList
+	 */
+	public List<Tuple2<String,String>> getAttributeValuePairList() {
+		return attributeValuePairList;
+	}
+
+	/**
+	 * @param attributeValuePairList the attributeValuePairList to set
+	 */
+	public void setAttributeValuePairList(List<Tuple2<String,String>> attributeValuePairList) {
+		this.attributeValuePairList = attributeValuePairList;
+	}
+
+	@Override
+	public String toString() {
+		return "BaseEntityImport [" + (code != null ? "code=" + code + ", " : "")
+				+ (name != null ? "name=" + name + ", " : "")
+				+ (attributeValuePairList != null ? "attributeValuePairList=" + attributeValuePairList : "") + "]";
+	}
+	
+	
+}
---
+++ src/main/java/life/genny/utils/ImportUtils.java
@@ -0,0 +1,100 @@
+package life.genny.utils;
+
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import org.apache.logging.log4j.Logger;
+
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import life.genny.bootxport.bootx.GoogleImportService;
+import life.genny.bootxport.bootx.XlsxImport;
+import life.genny.bootxport.bootx.XlsxImportOnline;
+import life.genny.models.BaseEntityImport;
+import life.genny.qwanda.Answer;
+import life.genny.qwandautils.QwandaUtils;
+
+public class ImportUtils {
+
+	/**
+	 * Stores logger object.
+	 */
+	protected static final Logger log = org.apache.logging.log4j.LogManager
+			.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
+
+	
+	public static List<BaseEntityImport> importGoogleDoc(final String id, String sheetName,Map<String,String> fieldMapping)
+	{		
+		List<BaseEntityImport> beImportList = new ArrayList<BaseEntityImport>();
+		log.info("Importing "+id);
+		   try {
+			   GoogleImportService gs = GoogleImportService.getInstance();
+			    XlsxImport xlsImport = new XlsxImportOnline(gs.getService());
+		//	    Realm realm = new Realm(xlsImport,id);
+//			    realm.getDataUnits().stream()
+//			        .forEach(data -> System.out.println(data.questions.size()));
+			    Set<String> keys = new HashSet<String>();
+			    for (String field : fieldMapping.keySet()) {
+			    	keys.add(field);
+			    }
+			      Map<String, Map<String,String>> mapData = xlsImport.mappingRawToHeaderAndValuesFmt(id, sheetName, keys);
+			      Integer rowIndex = 0;
+			      for (Map<String,String> row : mapData.values()) 
+			    	  
+			      {
+			    	  BaseEntityImport beImport = new BaseEntityImport();
+			    	  // generate a unique UUID
+			    	  String uniqueCodeField = row.get("UNIQUE_KEY_FIELD");
+			    	  String uniqueCode = null;
+			    	  String prefix = row.get("PREFIX");
+		    		  if (prefix == null) {
+		    			  prefix = "PER_";
+		    		  } else {
+		    			  prefix = prefix.toUpperCase();
+		    		  }
+
+			    	  if (uniqueCodeField == null) {
+			    		  uniqueCode = QwandaUtils.getUniqueId(prefix);	
+			    	  } else {
+			    		  uniqueCode = row.get(uniqueCodeField);
+				    	  if (uniqueCode == null) {
+				    		  uniqueCode = QwandaUtils.getUniqueId(prefix);	
+				    	  }		    	  
+				    	  else {
+				    		  uniqueCode = prefix+row.get(uniqueCodeField);
+				    		  uniqueCode = uniqueCode.toUpperCase();
+				    		  // remove non alpha digits
+				    		  uniqueCode = QwandaUtils.getNormalisedUsername(uniqueCode);
+				    	  }
+
+			    	  }
+			    	  beImport.setCode(uniqueCode);
+			    	  beImport.setName(uniqueCode);
+			    	  for (String col : row.keySet()) {
+			    		  String val = row.get(col.trim());
+			    		  if (val!=null) {
+			    			  val = val.trim();
+			    		  }
+			    		  String attributeCode = fieldMapping.get(col);
+			    		  // we now have attributeCode and the value
+			    		  Tuple2<String,String> pair = Tuple.of(attributeCode, val);
+			    		  beImport.getAttributeValuePairList().add(pair);
+			    		  
+			    	  }
+			    	  beImportList.add(beImport);
+			    	  rowIndex++;
+			      }
+			      
+			    } catch (Exception e1) {
+			      return beImportList;
+			    }
+
+		
+		return beImportList;
+	}
+}
