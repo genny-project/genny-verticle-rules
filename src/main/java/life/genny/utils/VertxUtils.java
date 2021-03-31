fa54af3e3ba5cc0a0f1500c35db0c84e0bc5d9c1
Adam Crow <adamcrow63@gmail.com>
2021-04-01 10:45:29 +1100

    Updted

--- src/main/java/life/genny/utils/VertxUtils.java
+++ src/main/java/life/genny/utils/VertxUtils.java
@@ -226,8 +226,10 @@ public class VertxUtils {
 
 				} else {
 					log.debug(" DDT URL:" + GennySettings.ddtUrl + ", realm:" + realm + "key:" + key + "token:" + token );
-//					resultStr = QwandaUtils.apiGet(GennySettings.ddtUrl + "/service/cache/read/" + realm + "/" + key, token);
-					resultStr = QwandaUtils.apiGet(GennySettings.ddtUrl + "/read/" + realm + "/" + key, token);	
+					resultStr = QwandaUtils.apiGet(GennySettings.ddtUrl + "/service/cache/read/" + realm + "/" + key, token);
+					if (resultStr==null) {
+						resultStr = QwandaUtils.apiGet(GennySettings.ddtUrl + "/read/" + realm + "/" + key, token);	
+					}
 					if (("<html><head><title>Error</title></head><body>Not Found</body></html>".equals(resultStr)) || ("<html><body><h1>Resource not found</h1></body></html>".equals(resultStr))) {
 						resultStr = QwandaUtils.apiGet(GennySettings.ddtUrl + "/service/cache/read/" + key, token);		
 					}
