9ded006a33f154ef56280472aef250e85640d344
ooanishoo <anish.maharjan12341@gmail.com>
2021-06-10 14:51:20 +1000

    Update VertxUtils.java

--- src/main/java/life/genny/utils/VertxUtils.java
+++ src/main/java/life/genny/utils/VertxUtils.java
@@ -826,6 +826,7 @@ public class VertxUtils {
 		}
 		// Handle Created and Updated attributes
 		if (Arrays.asList(filterAttributes).contains("PRI_CREATED")) {
+			log.info("filterAttributes contains PRI_CREATED");
 			System.out.println("Found PRI_CREATED");
 			Attribute createdAttr = new Attribute("PRI_CREATED", "Created", new DataType(LocalDateTime.class));
 			EntityAttribute created = new EntityAttribute(be, createdAttr, 1.0);
@@ -833,6 +834,7 @@ public class VertxUtils {
 			allowedAttributes.add(created);// allow attributes that starts with "LNK_"
 		}
 		if (Arrays.asList(filterAttributes).contains("PRI_UPDATED")) {
+			log.info("filterAttributes contains PRI_UPDATED");
 			System.out.println("Found PRI_UPDATED");
 			Attribute updatedAttr = new Attribute("PRI_UPDATED", "Updated", new DataType(LocalDateTime.class));
 			EntityAttribute updated = new EntityAttribute(be, updatedAttr, 1.0);
